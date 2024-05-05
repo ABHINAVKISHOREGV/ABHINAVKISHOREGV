d1e2032815e8fb3eb6597389428ce084c2b427dd
AbhinavkishoreGV <64400833+ABHINAVKISHOREGV@users.noreply.github.com>
2024-05-05 14:09:04 +0530

    Update k8sd.sh

--- k8sd.sh
+++ k8sd.sh
@@ -9,7 +9,6 @@ check_file() {
 
 case $ENV in
 	aura)
-
 		ENV='dev' check_file
 		cp ~/.kube/dev.yaml ~/.kube/config
 		;;
@@ -38,10 +37,31 @@ esac
 
 if [[ $I == 'false' ]]
 then
-	echo """alias k='kubectl'
-alias kd='kubectl -n protons'""" > ~/.kube/alias-cmds
-	mkdir -p "${HOME}/k8sconfig/rently-$ENV"
+	mkdir -p "${HOME}/k8sconfig/rently-$ENV" 
+	if [ ! -f "${HOME}/k8sconfig/rently-$ENV/.bashrc" ];then
+	echo """
+# ~/.bashrc: executed by bash(1) for non-login shells.
+
+# Note: PS1 and umask are already set in /etc/profile. You should not
+# need this unless you want different defaults for root.
+#PS1='${debian_chroot:+($debian_chroot)}\h:\w\$ '
+# umask 022
+
+# You may uncomment the following lines if you want 'ls' to be colorized:
+export LS_OPTIONS='--color=auto'
+#eval "$(dircolors)"
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
+#
+# Some more alias to avoid making mistakes:
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias k='kubectl' # Use k instead of kubectl 
+alias kd='kubectl -n protons' #Use kd instead of kubectl For DEV / AURA DEV Cluster""" > ${HOME}/k8sconfig/rently-$ENV/.bashrc
+	fi
+
 	cp -r "${HOME}/.kube" "${HOME}/k8sconfig/rently-$ENV/.kube"
-	docker run -it -h "rently-$ENV" --name "rently-$ENV"  -v "${HOME}/rently-$ENV":/root --rm abhinavkishoregv/kctl-debian:latest \
-		 bash -c "PATH=$PATH:/root/.local/bin;cp /root/.bashrc /root/.bashrc.bak; cat /root/.kube/alias-cmds >> /root/.bashrc && echo 'Use Kubectl commands just like your machine. You can also use the below aliases'; cat ~/.kube/alias-cmds && cd && bash"
+	docker run -it -h "rently-$ENV" --name "rently-$ENV"  -v "${HOME}/k8sconfig/rently-$ENV":/root --rm abhinavkishoregv/kctl-debian:latest bash -c "echo 'Use Kubectl commands just like your machine. You can also use the below aliases'; tail -n 2 ~/.bashrc && bash"
 fi
